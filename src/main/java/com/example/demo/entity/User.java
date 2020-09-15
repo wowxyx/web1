1b73534e8f9cccada29f303833ffeeb3f5cd31c2
wowxyx <1160377992@qq.com>
2020-09-15 11:08:34 +0800

    'Test1'

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+import org.omg.CORBA.INTERNAL;
+
+import java.util.Date;
+
+@Data
+public class User {
+
+    //@TableId(type = IdType.ID_WORKER) //mp自带策略，生成19位值，数字类型使用这种策略，比如long
+    //@TableId(type = IdType.ID_WORKER_STR) //mp自带策略，生成19位值，字符串类型使用这种策略
+    private int id;
+    private String name;
+    private Integer age;
+    private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @Version
+    @TableField(fill = FieldFill.INSERT)
+    private Integer version;
+
+    @TableLogic
+    private Integer deleted;
+
+
+}
